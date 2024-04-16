4f6ffe71d194689a47e2bc70c4867f58d4aecea2
Jimin Jang <dizzco@ltdmaster.com>
2024-04-16 16:39:04 +0900

    introduction to express

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+
+const home = (req, res, next) => {
+  return res.send("I love middleware");
+};
+const login = (req, res, next) => {
+  return res.send("login");
+};
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 👍`);
