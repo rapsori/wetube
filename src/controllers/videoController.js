c39e8ed60cd1ecc5f55a0b6eca6afe74eab82e6b
Jimin Jang <dizzco@ltdmaster.com>
2024-04-17 17:43:32 +0900

    pugging

---
+++ src/controllers/videoController.js
@@ -0,0 +1,14 @@
+export const trending = (req, res) => {
+    return res.render("home");
+};
+export const see = (req, res) => {
+    return res.render("watch");
+};
+export const edit = (req, res) => {
+    return res.render("edit");
+}
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
+    return res.send("Delete Video");
+}
--- src/server.js
+++ src/server.js
@@ -1,21 +1,22 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
+
+console.log(process.cwd());
 
 const app = express();
 const logger = morgan("dev");
 
-const home = (req, res, next) => {
-  return res.send("I love middleware");
-};
-const login = (req, res, next) => {
-  return res.send("login");
-};
-
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 👍`);
