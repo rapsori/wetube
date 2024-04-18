baf78ac03c610244c59f3e20a98b818d9492a9f3
Jimin Jang <dizzco@ltdmaster.com>
2024-04-18 10:34:53 +0900

    complete templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,40 @@
 export const trending = (req, res) => {
-    return res.render("home");
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const see = (req, res) => {
-    return res.render("watch");
+  return res.render("watch");
 };
 export const edit = (req, res) => {
-    return res.render("edit");
-}
+  return res.render("edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
-    return res.send("Delete Video");
-}
+  return res.send("Delete Video");
+};
