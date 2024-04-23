e91968bc5cf7a976bf9c4e57f70c7ebc29f37bb0
Jimin Jang <dizzco@ltdmaster.com>
2024-04-23 15:05:56 +0900

    complete user authentication

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -14,11 +15,14 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
-}));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
 
 app.use(localsMiddleware);
 app.use("/", rootRouter);
