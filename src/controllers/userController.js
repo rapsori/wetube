607fd18b94515e179c6e1a779133d241bef33298
Jimin Jang <dizzco@ltdmaster.com>
2024-04-23 17:36:17 +0900

    user profile ...ing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -109,8 +109,6 @@ export const finishGithubLogin = async (req, res) => {
   }
 };
 
-export const edit = (req, res) => res.send("Edit User");
-
 export const getLogin = (req, res) =>
   res.render("login", {
     pageTitle: "Login",
@@ -141,6 +139,45 @@ export const postLogin = async (req, res) => {
 
 export const logout = (req, res) => {
   req.session.destroy();
+  return res.redirect("/");
+};
+
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { name, email, username, location },
+  } = req;
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
+  req.session.user = updatedUser;
+  return res.redirect("/users/edit");
+};
+
+export const getChangePassword = (req, res) => {
+  if (req.session.user.socialOnly === true) {
+    return res.redirect("/");
+  }
+  return res.render("users/change-password", {
+    pageTitle: "Change Password",
+  });
+};
+
+export const postChangePassword = (req, res) => {
+  const { oldPassword, newPassword, newPasswordConfirmation } = req.body;
   return res.redirect("/");
 };
 
